0971821e7b1e910819fe29a65cfa0f26f91d1597
Zhou Xinzi <imdreamrunner@gmail.com>
2013-09-17 13:44:14 +0800

    Format Manager.java

--- app/models/Manager.java
+++ app/models/Manager.java
@@ -1,59 +1,61 @@
 package models;
 
-import java.util.*;
-import javax.persistence.*;
+import play.db.ebean.Model;
+import utils.CatException;
 
-import play.db.ebean.*;
-import play.data.format.*;
-import play.data.validation.*;
-
-import utils.*;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Entity
 public class Manager extends Model {
     @Id
     private Integer managerId;
-
     @Basic(optional = false)
     @Column(unique = true)
     private String username;
-
     private String password;
-
     private Integer type; // 0 for stall staffs, 1 for OFS.
-
     private Integer stallId;
 
     public static Finder<Integer, Manager> find = new Finder<Integer, Manager>(
         Integer.class, Manager.class
     );
 
-    public Integer getManagerId() {
-        return managerId;
+    public Manager(String newUsername, String newPassword, Integer newType, Integer newStallId) throws CatException {
+        setUsername(newUsername);
+        setPassword(newPassword);
+        setStallId(newStallId == null ? 0 : newStallId);
+        setType(newType == null? 0 : newType);
+        save();
     }
 
-    public String getUsername() {
-        return username;
+    public Integer getManagerId() { return managerId; }
+    public String getUsername() { return username; }
+    public Integer getType() { return type; }
+    public Integer getStallId() { return  stallId; }
+
+    public void setUsername(String newUsername) throws CatException {
+        if (newUsername == null || newUsername.length() < 5) {
+            throw new CatException(1, "Username must contain at least 5 characters.");
+        }
+        if (!checkUsername(newUsername)) {
+            throw new CatException(2, "Username has been taken.");
+        }
+        username  = newUsername;
     }
 
-    public void setUsername(String un) {
-        username  = un;
+    public void setPassword(String newPassword) throws CatException {
+        if (newPassword == null || newPassword.length() < 5) {
+            throw new CatException(3, "Password must contain at least 5 characters.");
+        }
+        password = newPassword;
     }
 
-    public void setPassword(String pw) {
-        password = pw;
-    }
-
-    public Integer getType() {
-        return type;
-    }
 
     public void setType(Integer t) {
         type = t;
-    }
-
-    public Integer getStallId() {
-        return  stallId;
     }
 
     public void setStallId(Integer newStallId) {
@@ -70,32 +72,6 @@ public class Manager extends Model {
         }
         Integer numOfUsers = find.where(String.format("username = '%s'", newUsername)).findRowCount();
         return numOfUsers == 0;
-    }
-
-//限制应该写在setter里面
-    public Manager(String newUsername, String newPassword, Integer newType, Integer newStallId) throws CatException {
-        if (newUsername == null || newUsername.length() < 5) {
-            throw new CatException(1, "Username must contain at least 5 characters.");
-        }
-        if (!checkUsername(newUsername)) {
-            throw new CatException(2, "Username has been taken.");
-        }
-        if (newPassword == null || newPassword.length() < 5) {
-            throw new CatException(3, "Password must contain at least 5 characters.");
-        }
-        username = newUsername;
-        setPassword(newPassword);
-        if (newStallId != null) {
-            stallId = newStallId;
-        } else {
-            stallId = 0;
-        }
-        if (newType != null) {
-            type = newType;
-        } else {
-            type = 0;
-        }
-        save();
     }
 
     public static Manager verifyManager(String username, String password) throws CatException {
