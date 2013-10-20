d2db0db4c98a713bf192449ebcaa44afca847448
LZQ <liuzhanqi0909@gmail.com>
2013-10-20 15:42:45 +0800

    ordercontroller, some comments

--- app/controllers/OrderController.java
+++ app/controllers/OrderController.java
@@ -13,4 +13,13 @@ public class OrderController extends Controller {
     public static Result viewMenu() {
         return ok(menu.render());
     }
+
+    public static Result calculateTotalPrice() {
+        return ok(menu.render());
+    }
 }
+
+
+
+# 一边点菜，一边在controller计算总价，并且传回coffee那边显示出来
+# 按pay button的时候，process payment
