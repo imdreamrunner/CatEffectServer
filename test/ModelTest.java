84a9599f889261d10630eccf049b592294d156a9
Zhou Xinzi <imdreamrunner@gmail.com>
2013-11-04 23:54:46 +0800

    Add account test

--- test/ModelTest.java
+++ test/ModelTest.java
@@ -1,2 +1,16 @@
+import models.Account;
+import org.junit.Test;
+
+import static junit.framework.Assert.assertNotNull;
+import static org.fest.assertions.Assertions.assertThat;
+
+
 public class ModelTest {
+    @Test
+    public void testAccount() {
+        Account account = new Account();
+        assertNotNull(account);
+        account.setType(1);
+        assertThat(account.getType()).isEqualTo(1);
+    }
 }
