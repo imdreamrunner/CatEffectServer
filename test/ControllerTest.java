0d00d0f1c9b84d9f4044153c7ee3edf4d989560a
Zhou Xinzi <imdreamrunner@gmail.com>
2013-11-05 11:56:06 +0800

    Edit controller test

--- test/ControllerTest.java
+++ test/ControllerTest.java
@@ -13,9 +13,27 @@ public class ControllerTest {
                 routes.ref.SystemController.dashboard()
         );
         assertThat(status(result)).isEqualTo(OK);
-        System.out.println(contentType(result));
         assertThat(contentType(result)).isEqualTo("text/html");
         assertThat(charset(result)).isEqualTo("utf-8");
         assertThat(contentAsString(result)).contains("Dashboard");
     }
+
+    /*
+    @Test
+    public void jsonAuth() {
+        Map<String,String> data = new HashMap<>();
+        data.put("auth_username", "admin");
+        data.put("auth_password", "admin");
+        Result result = callAction(
+                routes.ref.SystemController.auth(),
+                fakeRequest().withFormUrlEncodedBody(data)
+        );
+        assertThat(status(result)).isEqualTo(OK);
+        System.out.println(contentType(result));
+        assertThat(contentType(result)).isEqualTo("application/json");
+        assertThat(charset(result)).isEqualTo("utf-8");
+        System.out.println(contentAsString(result));
+        assertThat(contentAsString(result)).contains("admin");
+    }
+    */
 }
